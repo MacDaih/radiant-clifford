19669b2f36aa20d5de4affb9de410a81d716c439
MacDaih <gbrldavidson@gmail.com>
2021-04-12 23:25:19 +0200

    read tty from ino

--- embedded/daemon/main.go
+++ embedded/daemon/main.go
@@ -4,21 +4,25 @@ import (
 	"os"
 	"log"
 	"fmt"
+	"time"
 )
 
 func main() {
-	tty, err := os.Open("/dev/ttyACM0")
-	if err != nil {
-		log.Fatalf("can't open /dev/tty: %s", err)
+	for {
+		tty, err := os.Open("/dev/ttyACM0")
+		if err != nil {
+			log.Fatalf("can't open /dev/tty: %s", err)
+		}
+
+		b := make([]byte,512)
+
+		r, err := tty.Read(b)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Printf("Result => %s \n", string(b[:r]))
+		time.Sleep(time.Millisecond * 5000)
 	}
-
-	b := make([]byte,5)
-
-	r, err := tty.Read(b)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Printf("Result => %s \n", string(b[:r]))
 }
